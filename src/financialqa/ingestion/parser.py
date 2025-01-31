bfc1436db126e43ebe025140774c3e2d4bdef9a8
Ubuntu <messiahataey@gmail.com>
2025-01-31 22:10:34 +0000

    Update ingestion pipeline docstrings
    - Update class and method docstrings (Google style)
    - Update some function and variable names to be more descriptive
    - Make some minor formatting tweaks according to PEP 8

--- src/financialqa/ingestion/parser.py
+++ src/financialqa/ingestion/parser.py
@@ -14,72 +14,99 @@ To-do's:
     - Fix improper nested column headers
 """
 
-logging.basicConfig(    
-    # filename=logfile,    
-    level=logging.INFO,    
-    format="%(asctime)s %(levelname)s %(name)s line %(lineno)d  %(message)s",    
+logging.basicConfig(
+    # filename=logfile,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s line %(lineno)d  %(message)s",
     datefmt="%H:%M:%S"
-)   
+)
 logger = logging.getLogger(__name__)
 
 azure_docintel_key = os.getenv('AZURE_DOCINTEL_KEY')
 azure_docintel_endpoint = os.getenv('AZURE_DOCINTEL_ENDPOINT')
 azure_docintel_version = os.getenv('AZURE_DOCINTEL_VERSION')
 
-def parse_pdfs(pdf_paths):
+
+def extract_pdf_contents(pdf_paths):
+    """
+    Extract PDF contents using Azure Document Intelligence.
+
+    Args:
+        pdf_paths (list): List of PDF files to extract content from.
+
+    Returns:
+        dict: Dictionary containing extracted PDF contents. 
+    """
     document_intelligence_client = DocumentIntelligenceClient(
-        endpoint=os.environ['AZURE_DOCINTEL_ENDPOINT'], 
-        credential=AzureKeyCredential(os.environ['AZURE_DOCINTEL_KEY'])
+        endpoint=azure_docintel_endpoint,
+        credential=AzureKeyCredential(azure_docintel_key),
+        version=azure_docintel_version,
     )
-    parsed_pdfs_dict = {}
+    pdf_contents_dict = {}
     for pdf_path in pdf_paths:
         with open(pdf_path, "rb") as f:
             poller = document_intelligence_client.begin_analyze_document(
-                "prebuilt-layout", 
+                "prebuilt-layout",
                 body=f,
                 features=[DocumentAnalysisFeature.KEY_VALUE_PAIRS],
                 content_type="application/octet-stream",
             )
-            pdf_docintel_result_dict: AnalyzeResult = poller.result()    
+            pdf_analysis_results: AnalyzeResult = poller.result()
         pdf_name = pdf_path.split('/')[-1].replace('.pdf', '')
         pdf_name_parts = pdf_name.split('_')
         company_name = pdf_name_parts[0]
         report_quarter = pdf_name_parts[-1]
-        parsed_pdfs_dict[pdf_name] = {
-            'company_name': company_name, 
+        pdf_contents_dict[pdf_name] = {
+            'company_name': company_name,
             'report_quarter': report_quarter,
-            'result_dict': pdf_docintel_result_dict,
+            'docintel_analysis_results': pdf_analysis_results,
         }
-    return parsed_pdfs_dict
+    return pdf_contents_dict
 
-def page_pdf_contents(results_dict):
+
+def page_pdf_contents(pdf_contents_dict):
+    """
+    Create a dictionary of extracted PDF contents per page of each PDF.
+    
+    Args:
+        pdf_contents_dict (dict): Dictionary containing extracted PDF contents.
+
+    Returns:
+        dict: Dictionary containing extracted PDF contents per page of each PDF.
+    """
     pdf_pages_dict = dict()
-    for pdf_name, pdf_items in results_dict.items():
-        result_dict = pdf_items.get('result_dict')
-        company_name = pdf_items.get('company_name') 
-        report_quarter = pdf_items.get('report_quarter') 
-        text_table_chart_dict = {
-            'pages': {}, 
+    for pdf_name, pdf_items in pdf_contents_dict.items():
+        pdf_analysis_results = pdf_items.get('docintel_analysis_results')
+        company_name = pdf_items.get('company_name')
+        report_quarter = pdf_items.get('report_quarter')
+        pdf_contents = {
+            'pages': {},
             'company_name': company_name,
             'report_quarter': report_quarter,
         }
-        for paragraph in result_dict.get('paragraphs'):
+        for paragraph in pdf_analysis_results.get('paragraphs'):
             page_num = paragraph.get('boundingRegions')[0].get('pageNumber')
-            if page_num in text_table_chart_dict['pages'].keys():
-                text_table_chart_dict['pages'][page_num].get('text').append(paragraph.get('content'))
+            if page_num in pdf_contents['pages'].keys():
+                pdf_contents['pages'][page_num].get('text').append(
+                    paragraph.get('content'))
             else:
-                text_table_chart_dict['pages'][page_num] = {'text': [paragraph.get('content')], 'tables': []}
+                pdf_contents['pages'][page_num] = {
+                    'text': [paragraph.get('content')], 
+                    'tables': []
+                }
             if 'role' in paragraph.keys():
-                if paragraph['role'] in text_table_chart_dict['pages'][page_num].keys():
-                    text_table_chart_dict['pages'][page_num][paragraph['role']].append(paragraph.get('content'))
+                if paragraph['role'] in pdf_contents['pages'][page_num].keys():
+                    pdf_contents['pages'][page_num][paragraph['role']].append(
+                        paragraph.get('content'))
                 else:
-                    text_table_chart_dict['pages'][page_num][paragraph['role']] = [paragraph.get('content')]
+                    pdf_contents['pages'][page_num][paragraph['role']] = [
+                        paragraph.get('content')]
                 # Remove duplicate text roles from text
-                for role in text_table_chart_dict['pages'][page_num][paragraph['role']]:
-                    if role in text_table_chart_dict['pages'][page_num]['text']:
-                        text_table_chart_dict['pages'][page_num]['text'].remove(role)
+                for role in pdf_contents['pages'][page_num][paragraph['role']]:
+                    if role in pdf_contents['pages'][page_num]['text']:
+                        pdf_contents['pages'][page_num]['text'].remove(role)
 
-        for table in result_dict.get('tables'):
+        for table in pdf_analysis_results.get('tables'):
             page_num = table.get('boundingRegions')[0].get('pageNumber')
             row_count = table['rowCount']
             column_count = table['columnCount']
@@ -87,8 +114,8 @@ def page_pdf_contents(results_dict):
             arr[0][:] = ''
             for cell in table['cells']:
                 # Remove duplicate table cell values from text
-                if cell['content'] in text_table_chart_dict['pages'][page_num]['text']:
-                    text_table_chart_dict['pages'][page_num]['text'].remove(cell['content'])
+                if cell['content'] in pdf_contents['pages'][page_num]['text']:
+                    pdf_contents['pages'][page_num]['text'].remove(cell['content'])
                 if 'kind' in cell.keys():
                     # Handles nested headers
                     if cell['kind'] == 'columnHeader':
@@ -108,35 +135,33 @@ def page_pdf_contents(results_dict):
             df.reset_index(inplace=True, drop=True)
             df.dropna(inplace=True)
             df = df.to_dict(orient="records")
-            if page_num in text_table_chart_dict['pages'].keys():
-                text_table_chart_dict['pages'][page_num].get('tables').append(df)
+            if page_num in pdf_contents['pages'].keys():
+                pdf_contents['pages'][page_num].get('tables').append(df)
             else:
-                text_table_chart_dict['pages'][page_num] = {'text': [], 'tables': [df]}
-        pdf_pages_dict.update({pdf_name: text_table_chart_dict})
+                pdf_contents['pages'][page_num] = {'text': [], 'tables': [df]}
+        pdf_pages_dict.update({pdf_name: pdf_contents})
 
-        for figures in result_dict.get('figures'):
+        for figures in pdf_analysis_results.get('figures'):
             figure_bounding_regions = figures.get('boundingRegions')
             for i, bounding_regions in enumerate(
                     figure_bounding_regions, start=1
                     ):
                 bounding_regions_polygon = bounding_regions.get('polygon')
                 page_num = bounding_regions.get('pageNumber')
-                if page_num not in text_table_chart_dict.get('pages').keys():
-                    text_table_chart_dict['pages'].update({page_num: {'figures': {}}})
-                elif 'figures' not in text_table_chart_dict.get('pages').get(page_num):
-                    text_table_chart_dict['pages'].get(page_num).update({'figures': {}})
+                if page_num not in pdf_contents.get('pages').keys():
+                    pdf_contents['pages'].update({page_num: {'figures': {}}})
+                elif 'figures' not in pdf_contents.get('pages').get(page_num):
+                    pdf_contents['pages'].get(page_num).update({'figures': {}})
                 figure_file_name = f'figure_{i}_page_{page_num}'
                 if 'caption' in figures.keys():
-                    text_table_chart_dict['pages'][page_num].get('figures').update({
+                    pdf_contents['pages'][page_num].get('figures').update({
                         figure_file_name: {
                             'bounding_regions_polygon': bounding_regions_polygon,
                             'caption': figures.get('caption').get('content'),
-                        }
-                    })
+                        }})
                 else:
-                    text_table_chart_dict['pages'][page_num]['figures'].update({
+                    pdf_contents['pages'][page_num]['figures'].update({
                         figure_file_name: {
                             'bounding_regions_polygon': bounding_regions_polygon,
-                        }
-                    })
+                        }})
     return pdf_pages_dict
